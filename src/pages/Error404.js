499dbc0df40b0d1149d5fd9acf0977fac511449d
César Rivas <61394656+csarrvas@users.noreply.github.com>
2020-05-30 09:37:44 -0600

    error404 page fixed
    error404 style added

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles/error404.scss';
 import error from '../assets/images/error-404.png';
+import './styles/error404.scss';
 
 const Error404 = () => {
   return (
     <div id="error404">
-      <img id="error-image" alt="error 404" src={error} />
-      <p id="error-message">Page not found</p>
+      <img alt="error 404" src={error} />
+      <p>Page not found</p>
     </div>
   );
 }
